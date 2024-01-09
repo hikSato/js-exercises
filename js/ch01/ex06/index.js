e87a436000ab52be398a875a1a0a4d8fd85ab2fe
hiksato <Hikaru.Sato2@jp.ricoh.com>
2024-01-09 18:58:50 +0900

    Fix code

--- js/ch01/ex06/index.js
+++ js/ch01/ex06/index.js
@@ -1,5 +1,10 @@
+const map = new Map();
+
 export const fib = (value) => {
   if (value === 0) return 0;
   if (value === 1) return 1;
-  return fib(value - 1) + fib(value - 2);
+  if (map.has(value)) return map.get(value);
+  const result = fib(value - 1) + fib(value - 2);
+  map.set(value, result);
+  return result;
 };
