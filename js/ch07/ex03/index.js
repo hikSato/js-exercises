55825a445aed9f6db8bd5b6954cf21b378b2b599
hiksato <hiksato2@gmail.com>
2024-03-21 00:57:09 +0900

    Add code

--- js/ch07/ex03/index.js
+++ js/ch07/ex03/index.js
@@ -15,14 +15,19 @@ export const join = (arr, separator = ",") => {
 
 export const reverse = (arr) => {
   if (!arr) throw Error();
-  const reverseArr = new Array(arr.length);
-  return arr.reduce((a, b, i, o) => (reverseArr[o.length - i] = a), reverseArr);
+  return arr.reduce((a, b) => [b, ...a], []);
 };
 
 export const every = (arr, isBelowThreshold) => {
-  return arr.reduce((a, b) => a && isBelowThreshold(b), true);
+  return arr.reduce(
+    (a, b, index, arr) => a && isBelowThreshold(b, index, arr),
+    true
+  );
 };
 
 export const some = (arr, isBelowThreshold) => {
-  arr.reduce((a, b) => a || isBelowThreshold(b), false);
+  return arr.reduce(
+    (a, b, index, arr) => a || isBelowThreshold(b, index, arr),
+    false
+  );
 };
