a3236beafcf705e4177df38cf079973486a942a0
hiksato <Hikaru.Sato2@jp.ricoh.com>
2024-03-22 09:19:24 +0900

    Fix code

--- js/ch06/ex11/index.js
+++ js/ch06/ex11/index.js
@@ -1,6 +1,6 @@
 export const obj = {
   r: 1,
-  theta: (2 * Math.PI) / 3,
+  theta: Math.PI / 4,
 
   get x() {
     return this.r * Math.cos(this.theta);
@@ -10,12 +10,12 @@ export const obj = {
   },
 
   set x(value) {
-    if (isNaN(value)) new Error();
+    if (isNaN(value)) throw new Error();
     this.r = Math.sqrt(value ** 2 + this.y ** 2);
     this.theta = Math.atan2(this.y, value);
   },
   set y(value) {
-    if (isNaN(value)) new Error();
+    if (isNaN(value)) throw new Error();
     this.r = Math.sqrt(this.x ** 2 + value ** 2);
     this.theta = Math.atan2(value, this.x);
   },
