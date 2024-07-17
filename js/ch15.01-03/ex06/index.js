02023a3fd6fe9da123f7de1516b8d1043e78a718
hikSato <64115509+hikSato@users.noreply.github.com>
2024-07-17 09:12:29 +0900

    Fix code

--- js/ch15.01-03/ex06/index.js
+++ js/ch15.01-03/ex06/index.js
@@ -1,11 +1,4 @@
 document.getElementById("date").textContent = new Date().toISOString();
 document.getElementById("agent").textContent = navigator.userAgent;
-document.getElementById("status").textContent = navigator.onLine
-  ? "オンライン"
-  : "オフライン";
 document.getElementById("language").textContent = navigator.language;
-// document.getElementById("resolution").textContent = navigator.add;
-// document.getElementById("pixel").textContent = navigator.add;
-// document.getElementById("cookie").textContent = navigator.add;
-// document.getElementById("jsEnabled").textContent = navigator.add;
 document.getElementById("url").textContent = window.location.href;
