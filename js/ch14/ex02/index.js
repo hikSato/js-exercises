49f5742caec29661e9538ae97db727def7efee1b
hikSato <64115509+hikSato@users.noreply.github.com>
2024-07-19 14:12:05 +0900

    Fix code

--- js/ch14/ex02/index.js
+++ js/ch14/ex02/index.js
@@ -1,44 +1,15 @@
 export class MyArrayLike {
-  constructor(items = []) {
-    this.items = items;
-  }
-
-  // Add a length property to mimic array behavior
-  get length() {
-    return this.items.length;
-  }
-
-  [Symbol.iterator]() {
-    return this.items[Symbol.iterator]();
-  }
-
-  push(item) {
-    this.items.push(item);
-  }
-
-  toArray() {
-    return this.items;
+  constructor(length) {
+    this.length = length;
   }
 }
 
 export class MyArray extends Array {
-  static get [Symbol.species]() {
-    return MyArray;
-  }
-
-  constructor(...items) {
+  constructor(items) {
     super(...items);
   }
 
-  // Override map to return MyArrayLike
-  map(callback, thisArg) {
-    const result = super.map(callback, thisArg);
-    return new MyArrayLike(result);
-  }
-
-  // Override slice to return MyArrayLike
-  slice(...args) {
-    const result = super.slice(...args);
-    return new MyArrayLike(result);
+  static get [Symbol.species]() {
+    return MyArrayLike;
   }
 }
