2d40f32c6e017d25fead266f92e4d9b348d4682e
hikSato <64115509+hikSato@users.noreply.github.com>
2024-09-17 11:35:11 +0900

    Fix code

--- js/ch15.04-10/ex08/clock.js
+++ js/ch15.04-10/ex08/clock.js
@@ -7,7 +7,18 @@
   let minangle = min * 6;
   let hourangle = hour * 30;
 
-  let secondhand = document.querySelector("#clock .secondhand");
+  // let secondhand = document.querySelector("#clock .secondhand");
+  const secondhand =
+    document.querySelector("#clock .secondhand") ??
+    document.createElementNS("http://www.w3.org/2000/svg", "line");
+  secondhand.setAttribute("class", "secondhand");
+  secondhand.setAttribute("x1", "50");
+  secondhand.setAttribute("y1", "50");
+  secondhand.setAttribute("x2", "50");
+  secondhand.setAttribute("y2", "15");
+  secondhand.setAttribute("stroke", "red");
+  document.querySelector("#clock").appendChild(secondhand);
+
   let minhand = document.querySelector("#clock .minutehand");
   let hourhand = document.querySelector("#clock .hourhand");
 
